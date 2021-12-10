15e7ddbc81adfaf9a49404b31a131b9ce202b288
Vitor Murbach <vitormurbach626@gmail.com>
2021-12-09 22:54:46 -0300

    Primeiro site com angular criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './view/contato/contato.component';
+import { HomeComponent } from './view/home/home.component';
+import { JogosComponent } from './view/jogos/jogos.component';
+import { PromocoesComponent } from './view/promocoes/promocoes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'contato', component: ContatoComponent},
+  { path: 'jogos', component: JogosComponent},
+  { path: 'promocoes', component: PromocoesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './view/header/header.component';
+import { HomeComponent } from './view/home/home.component';
+import { FooterComponent } from './view/footer/footer.component';
+import { ContatoComponent } from './view/contato/contato.component';
+import { JogosComponent } from './view/jogos/jogos.component';
+import { PromocoesComponent } from './view/promocoes/promocoes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    ContatoComponent,
+    JogosComponent,
+    PromocoesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
